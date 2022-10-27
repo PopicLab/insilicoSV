02297b270c490d6a91a3545a4bbb656b20e2287f
crohlicek <chris@rohlicek.com>
2022-10-27 16:35:01 -0400

    Commenting out code that changed TRA to be A_->_A rather than reciprocal (currently regenerating a genome with TRAs of the old form)

--- structural_variant.py
+++ structural_variant.py
@@ -113,11 +113,12 @@ class Structural_Variant():
         # symbols_dict[Symbols.PLACEHOLDER] = (0, (0,0))
 
         # if we're operating on a TRA, flip a coin to set the A or B interval to be of size 0
-        if self.type == Variant_Type.TRA:
-            if random.randint(0, 1):
-                symbols_dict['A'] = (0, (0, 0))
-            else:
-                symbols_dict['B'] = (0, (0, 0))
+        # REGENERATING OLD TRAINING GENOME, WHICH USED THE OLD DEFINITION OF TRANSLOCATION (THAT THIS NEGATED)
+        # if self.type == Variant_Type.TRA:
+        #     if random.randint(0, 1):
+        #         symbols_dict['A'] = (0, (0, 0))
+        #     else:
+        #         symbols_dict['B'] = (0, (0, 0))
 
         # initialize event classes
         for idx, symbol in enumerate(all_symbols):
