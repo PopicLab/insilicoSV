42ee953039d0b307c9ba6b61668e055dc2e06265
crohlicek <chris@rohlicek.com>
2022-11-17 10:20:43 -0500

    Tweak div. repeat script -- addition of vcf merging and mixed-type position correction to fix_div_dDUP_vcf.py

--- fix_div_dDUP_vcf.py
+++ fix_div_dDUP_vcf.py
@@ -16,11 +16,13 @@ def correct_positions(input_vcf, label_entire_event=False):
     # --> to be added to the positions in a given vcf record
     total_ins_len = 0
     for pos in sorted(vcf_recs.keys()):
+        evt = vcf_recs[pos]
         # *** shifting the start position of a vcf record automatically shifts the stop position as well
-        vcf_recs[pos].start += total_ins_len
-        # vcf_recs[pos].stop += total_ins_len
-        vcf_recs[pos].info['TARGET'] += total_ins_len
-        total_ins_len += vcf_recs[pos].info['SVLEN']
+        evt.start += total_ins_len
+        # in case of mixed vcf (div_dDUPs and other types) only want to shift events by cumulative div_dDUP length
+        if evt.id == 'div_dDUP':
+            evt.info['TARGET'] += total_ins_len
+            total_ins_len += evt.info['SVLEN']
 
     for rec in vcf_recs.values():
         if not label_entire_event:
@@ -36,9 +38,24 @@ def correct_positions(input_vcf, label_entire_event=False):
     in_vcf.close()
 
 
+def incorporate_events(merge_vcf_path, div_vcf_path, output_vcf_path):
+    div_vcf = VariantFile(div_vcf_path)
+    merge_vcf = VariantFile(merge_vcf_path)
+    # going to use the header from the div_vcf -- I'm not sure if in general one is preferable over the other
+    # but in the benchmarks so far it seems they'll usually be the same
+    hd = div_vcf.header
+    out_vcf = VariantFile(output_vcf_path, 'w', header=hd)
+    for vcf in [div_vcf, merge_vcf]:
+        for rec in vcf.fetch():
+            out_vcf.write(rec)
+    out_vcf.close()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Correct event positions in vcf recording div_dDUPs')
-    parser.add_argument('--input_vcf', help='Input vcf from R1 generation step')
+    parser.add_argument('--div_dDUP_vcf', help='Input vcf from R1 generation step')
+    parser.add_argument('--merge_input_vcf', help='Input vcf with previously simulated events to merge into div. vcf')
+    parser.add_argument('--merge_output_vcf', help='Output path for merged div. dDUP / other type vcf')
     parser.add_argument('--label_entire_event', action='store_true', help='Boolean flag for whether or not'
                                                                           'output vcf should report one record '
                                                                           'per div. repeat with start/end giving '
@@ -47,4 +64,5 @@ if __name__ == "__main__":
                                                                           'specifically)')
     args = parser.parse_args()
 
-    correct_positions(args.input_vcf, args.label_entire_event)
+    incorporate_events(args.merge_input_vcf, args.div_dDUP_vcf, args.merge_output_vcf)
+    correct_positions(args.merge_output_vcf, args.label_entire_event)
