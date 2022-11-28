02076ac6f7f184b4e3f5c172dec2f3cbda2d9ed2
crohlicek <chris@rohlicek.com>
2022-11-28 14:18:49 -0500

    typo in edit_ref

--- edit_ref.sh
+++ edit_ref.sh
@@ -1,11 +1,11 @@
 # (recreating reference-editing script that I must have left on Hendrix)
 SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPT_PATH=$(dirname "$SCRIPT")
 CONFIG=$1 #<- simulation config for simulate.py
 FASTA_PATH=$2 #<- reference to be used as base for simulation
 OUTPUT_PREFIX=$3 #<- filename prefix for output files (should include path to output dir)
 
-python ${SCRIPTPATH}/simulate.py ${FASTAPATH} ${CONFIG} ${OUTPUT_PREFIX}
+python ${SCRIPT_PATH}/simulate.py ${FASTA_PATH} ${CONFIG} ${OUTPUT_PREFIX}
 dwgsim -C 30 -1 151 -2 151 -y 0 -S 0 -c 0 -m /dev/null -H ${OUTPUT_PREFIX}.hapA.fa ${OUTPUT_PREFIX}.dwgsim.hap.0
 dwgsim -C 30 -1 151 -2 151 -y 0 -S 0 -c 0 -m /dev/null -H ${OUTPUT_PREFIX}.hapB.fa ${OUTPUT_PREFIX}.dwgsim.hap.1
 mv ${OUTPUT_PREFIX}.dwgsim.hap.0.bfast.fastq.gz ${OUTPUT_PREFIX}.dwgsim.hap.12.bfast.fastq.gz
