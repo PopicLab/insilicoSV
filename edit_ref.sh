573686e0eeb3dec397ffffc4684e06030736dd4f
crohlicek <chris@rohlicek.com>
2022-12-07 11:55:28 -0500

    Set edit_ref.sh to point to optimized dwgsim on cornell cluster

--- edit_ref.sh
+++ edit_ref.sh
@@ -8,17 +8,13 @@ OUTPUT_PREFIX=$3 #<- filename prefix for output files (should include path to ou
 python ${SCRIPT_PATH}/simulate.py ${FASTA_PATH} ${CONFIG} ${OUTPUT_PREFIX}
 # create correctly-formatted vcf for dDUPs+simple events (remove step if not simulating dDUPs or TRAs)
 python ${SCRIPT_PATH}/complex_bed_to_vcf.py --bed_file ${OUTPUT_PREFIX}.bed --vcf_file ${OUTPUT_PREFIX}_dDUPdoublelabel.vcf --template_vcf ${OUTPUT_PREFIX}.vcf --include_simple DEL DUP INV
-dwgsim -C 30 -1 151 -2 151 -y 0 -S 0 -c 0 -m /dev/null -H ${OUTPUT_PREFIX}.hapA.fa ${OUTPUT_PREFIX}.dwgsim.hap.0
-dwgsim -C 30 -1 151 -2 151 -y 0 -S 0 -c 0 -m /dev/null -H ${OUTPUT_PREFIX}.hapB.fa ${OUTPUT_PREFIX}.dwgsim.hap.1
-mv ${OUTPUT_PREFIX}.dwgsim.hap.0.bfast.fastq.gz ${OUTPUT_PREFIX}.dwgsim.hap.12.bfast.fastq.gz
-cat ${OUTPUT_PREFIX}.dwgsim.hap.1.bfast.fastq.gz >> ${OUTPUT_PREFIX}.dwgsim.hap.12.bfast.fastq.gz
-rm ${OUTPUT_PREFIX}.dwgsim.hap.1.bfast.fastq.gz
-bwa mem -t20 -p ${FASTA_PATH} ${OUTPUT_PREFIX}.dwgsim.hap.12.bfast.fastq.gz | samtools view -Sb - > ${OUTPUT_PREFIX}.bwamem.bam
-rm ${OUTPUT_PREFIX}.dwgsim.hap.12.bfast.fastq.gz
+/athena/ihlab/scratch/vpopic/software/LRSIM/dwgsim -C 30 -1 151 -2 151 -y 0 -S 0 -c 0 -m /dev/null -H ${OUTPUT_PREFIX}.hapA.fa ${OUTPUT_PREFIX}.dwgsim.hap.0
+/athena/ihlab/scratch/vpopic/software/LRSIM/dwgsim -C 30 -1 151 -2 151 -y 0 -S 0 -c 0 -m /dev/null -H ${OUTPUT_PREFIX}.hapB.fa ${OUTPUT_PREFIX}.dwgsim.hap.1
+mv ${OUTPUT_PREFIX}.dwgsim.hap.0.12.fastq ${OUTPUT_PREFIX}.dwgsim.hap.12.fastq
+cat ${OUTPUT_PREFIX}.dwgsim.hap.1.12.fastq >> ${OUTPUT_PREFIX}.dwgsim.hap.12.fastq
+rm ${OUTPUT_PREFIX}.dwgsim.hap.1.12.fastq
+bwa mem -t20 -p ${FASTA_PATH} ${OUTPUT_PREFIX}.dwgsim.hap.12.fastq | samtools view -Sb - > ${OUTPUT_PREFIX}.bwamem.bam
+rm ${OUTPUT_PREFIX}.dwgsim.hap.12.fastq
 samtools sort -@ 20 ${OUTPUT_PREFIX}.bwamem.bam > ${OUTPUT_PREFIX}.bwamem.sorted.bam
 rm ${OUTPUT_PREFIX}.bwamem.bam
 samtools index -@ 20 ${OUTPUT_PREFIX}.bwamem.sorted.bam
-
-# NB: if running on Gibus, use optimized DWSGIM for read generation: /athena/ihlab/scratch/vpopic/software/LRSIM/dwgsim
-# ---> and if using that version, need to correct read files to be of the form:
-# ${OUTPUT_PREFIX}.dwgsim.hap.{0,1}.12.fastq (i.e., no .gz suffix, and no .bfast tag)
