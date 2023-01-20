d669202a7abf147eae2a48e1b5df0a514a2da815
crohlicek <chris@rohlicek.com>
2023-01-20 17:02:21 -0500

    updating edit_ref.sh to skip the call to complex_bed_to_vcf.py

--- edit_ref.sh
+++ edit_ref.sh
@@ -6,8 +6,6 @@ FASTA_PATH=$2 #<- reference to be used as base for simulation
 OUTPUT_PREFIX=$3 #<- filename prefix for output files (should include path to output dir)
 
 python ${SCRIPT_PATH}/simulate.py ${FASTA_PATH} ${CONFIG} ${OUTPUT_PREFIX}
-# create correctly-formatted vcf for dDUPs+simple events (remove step if not simulating dDUPs or TRAs)
-python ${SCRIPT_PATH}/complex_bed_to_vcf.py --bed_file ${OUTPUT_PREFIX}.bed --vcf_file ${OUTPUT_PREFIX}_dDUPdoublelabel.vcf --template_vcf ${OUTPUT_PREFIX}.vcf --include_simple DEL DUP INV
 /athena/ihlab/scratch/vpopic/software/LRSIM/dwgsim -C 30 -1 151 -2 151 -y 0 -S 0 -c 0 -m /dev/null -H ${OUTPUT_PREFIX}.hapA.fa ${OUTPUT_PREFIX}.dwgsim.hap.0
 /athena/ihlab/scratch/vpopic/software/LRSIM/dwgsim -C 30 -1 151 -2 151 -y 0 -S 0 -c 0 -m /dev/null -H ${OUTPUT_PREFIX}.hapB.fa ${OUTPUT_PREFIX}.dwgsim.hap.1
 mv ${OUTPUT_PREFIX}.dwgsim.hap.0.12.fastq ${OUTPUT_PREFIX}.dwgsim.hap.12.fastq
