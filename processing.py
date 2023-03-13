68af0418d7c3b4717b1f4ef70cd9691a411f05b9
crohlicek <chris@rohlicek.com>
2023-03-13 14:29:28 -0400

    Consolidating INS logic into other simple event cases

--- processing.py
+++ processing.py
@@ -171,8 +171,8 @@ class FormatterIO():
             # create target events dict for lookup of corresponding source/target events within SV
             # SVs with multiple source events will be split into multiple bed records (one for each)
             if len(sv.events_dict) == 1:
-                # DEL/DUP/INV/INS; getting operation using symbol logic in composite event helper fn
-                ev = list(sv.events_dict.values())[0]
+                ev = list(sv.sv_blocks.target_events_dict.values())[0] if sv.type == Variant_Type.INS\
+                        else list(sv.events_dict.values())[0]
                 op = self.get_event_target_operation(ev.symbol, sv.sv_blocks.target_events_dict, sv.events_dict)[1]
                 record_info = {'source_s': ev.start, 'source_e': ev.end, 'target_s': ev.start, 'target_e': ev.end,
                                'transform': op, 'sv': sv, 'event': ev, 'bedfile': bedfile}
