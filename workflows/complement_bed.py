1ebd0c22aad705152de0a0051b56195cd430db71
ebattistella <en.battistella@gmail.com>
2025-07-18 01:07:16 +0200

    * Script to reverse BED file annotations to get the interval between regions. * Add a use case for inversions within segmental duplications

---
+++ workflows/complement_bed.py
@@ -0,0 +1,39 @@
+import argparse
+from collections import defaultdict
+
+def complement_bed(bed, complement_path=None):
+    if not complement_path:
+        complement_path = bed.rsplit('.', 1)[0] + '_complement.bed'
+
+    regions = defaultdict(list)
+    with open(bed, 'r') as f:
+        for line in f.readlines():
+            line = line.split('\t')
+            regions[line[0]].append(line)
+
+    complement = defaultdict(list)
+    for chrom, chrom_regions in regions.items():
+        chrom_regions.sort(key=lambda x: x[1])
+        for idx, region in enumerate(chrom_regions[1:]):
+            previous_region = chrom_regions[idx]
+            if previous_region[2] >= region[1]:
+                raise(Exception(f'Region {region} overlaps previous region {previous_region}.'
+                                f'Complement is not defined for overlapping regions.'))
+            complement[region[0]].append([region[0], previous_region[2], region[1], 'complement'])
+
+    with open(complement_path, 'w') as f:
+        f.write('\t'.join(complement) + '\n')
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-b', '--bed', required=True, help='Input BED file to complement')
+    parser.add_argument('-o', '--output', required=False, help='Complemented output BED file path')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    complement(args.bed, args.output)
