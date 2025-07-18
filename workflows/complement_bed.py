34677bca3515c9d4473ffe098ccf6268441aa6c9
ebattistella <en.battistella@gmail.com>
2025-07-18 16:13:18 +0200

    * Correct input output

--- workflows/complement_bed.py
+++ workflows/complement_bed.py
@@ -8,23 +8,19 @@ def complement_bed(bed, complement_path=None):
     regions = defaultdict(list)
     with open(bed, 'r') as f:
         for line in f.readlines():
-            line = line.split('\t')
+            line = line.strip().split()
             regions[line[0]].append(line)
 
-    complement = defaultdict(list)
-    for chrom, chrom_regions in regions.items():
-        chrom_regions.sort(key=lambda x: x[1])
-        for idx, region in enumerate(chrom_regions[1:]):
-            previous_region = chrom_regions[idx]
-            if previous_region[2] >= region[1]:
-                raise(Exception(f'Region {region} overlaps previous region {previous_region}.'
-                                f'Complement is not defined for overlapping regions.'))
-            complement[region[0]].append([region[0], previous_region[2], region[1], 'complement'])
-
     with open(complement_path, 'w') as f:
-        f.write('\t'.join(complement) + '\n')
-
-
+        for chrom, chrom_regions in regions.items():
+            chrom_regions.sort(key=lambda x: int(x[1]))
+            for idx, region in enumerate(chrom_regions[1:]):
+                previous_region = chrom_regions[idx]
+                if int(previous_region[2]) > int(region[1]):
+                    raise(Exception(f'Region {region} overlaps previous region {previous_region}. '
+                                    f'Complement is not defined for overlapping regions.'))
+                if previous_region[2] == region[1]: continue
+                f.write('\t'.join([region[0], previous_region[2], region[1], 'complement']) + '\n')
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -36,4 +32,4 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    complement(args.bed, args.output)
+    complement_bed(args.bed, args.output)
